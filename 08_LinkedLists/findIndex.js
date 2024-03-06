845c985b1d4586f79900d132501bc407131aa63f
Amber Jankowski <amberbjankowski@gmail.com>
2024-03-06 16:28:23 -0500

    fixed it to return null instead of false incase index doesn't exist

--- 08_LinkedLists/findIndex.js
+++ 08_LinkedLists/findIndex.js
@@ -17,13 +17,13 @@ c.next = d;
 //set count
 //create function that accepts head and index as arguments
 //check if count is equal to head, if so return head.val
-//if head is equal to null return false
+//if head is equal to null return null
 //final return statment should be a call to the same function to make it recursive
 
 let count = 0;
 function findIndex(head, index) {
   if (count === index) return head.val;
-  if (head === null) return false;
+  if (head === null) return null;
   count++;
   return findIndex(head.next, index);
 }
@@ -33,7 +33,7 @@ console.log(findIndex(a, 2));
 //create a function that accepts head and index as arguments
 //create a count variable set to 0
 //create a current variable set to head
-//if current is equal to null return false
+//if current is equal to null return null
 //while current is not equal to null, check if index matches count
 //if match return current.val
 //else current = current.next
