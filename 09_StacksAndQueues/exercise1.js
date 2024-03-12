a85b751a98f5d67363a7c728006a7d1472fdb7e2
Amber Jankowski <amberbjankowski@gmail.com>
2024-03-12 10:41:35 -0400

    started stacks exercise, template complete

---
+++ 09_StacksAndQueues/exercise1.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {}
+  push(value) {}
+  pop() {}
+}
