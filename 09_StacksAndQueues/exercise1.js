a69697aca1fbb672476753c15aa547417af767bd
Amber Jankowski <amberbjankowski@gmail.com>
2024-03-12 11:04:43 -0400

    completed peek,push and pop methods for stacks and linked lists

--- 09_StacksAndQueues/exercise1.js
+++ 09_StacksAndQueues/exercise1.js
@@ -1,7 +1,7 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = next;
+    this.next = null;
   }
 }
 
@@ -11,7 +11,39 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
-  peek() {}
-  push(value) {}
-  pop() {}
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
 }
+
+const myStack = new Stack();
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("Discord");
+myStack.pop();
+console.log(myStack);
+console.log(myStack.peek());
